a6a4ddcad3a547f4404a3dd924690c00263bf60b
Kasper <kasper@Kaspers-MacBook-Air.local>
2018-02-19 15:03:27 -0500

    initial commit

---
+++ src/main/java/com/blakerodgers/musicreviews/MusicReviewsApplication.java
@@ -0,0 +1,12 @@
+package com.blakerodgers.musicreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicReviewsApplication.class, args);
+	}
+}
